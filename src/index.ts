8b77802051155f1eab00594a0e6058f3e0cb0c16
syaiful <143513644+Syaiful313@users.noreply.github.com>
2024-12-18 17:55:35 +0700

    feat: blog list (#5)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from "express";
 import { PORT } from "./config";
 import sampleRouter from "./routes/sample.router"
 import authRouter from "./routes/auth.router"
+import blogRouter from "./routes/blog.router"
 import cors from "cors"
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json())
 
 app.use("/samples", sampleRouter)
 app.use("/auth", authRouter)
+app.use("/blogs", blogRouter)
 
 
 // middleware error 
