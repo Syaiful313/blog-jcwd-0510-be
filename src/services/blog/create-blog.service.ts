146771030952a41d212fd4d81733d1cd0e83698d
Syaiful313 <mthitz313@gmail.com>
2024-12-19 11:49:11 +0700

    feat: create blog

---
+++ src/services/blog/create-blog.service.ts
@@ -0,0 +1,40 @@
+import { Blog } from "@prisma/client";
+import prisma from "../../lib/prisma";
+import { cloudinaryUpload } from "../../lib/cloudinary";
+
+interface CreateBlogBody {
+    title: string;
+    description: string;
+    category: string;
+    content: string;
+}
+
+export const createBlogService = async (
+  body: CreateBlogBody,
+  thumbnail: Express.Multer.File,
+  userId: number
+) => {
+  try {
+    const { title, description, category, content } = body;
+
+    const blog = await prisma.blog.findFirst({
+      where: { title },
+    });
+
+    if(blog){
+      throw new Error("Blog already exists");
+    }
+
+    const {secure_url} = await cloudinaryUpload(thumbnail);
+
+    await prisma.blog.create({
+      data: {
+        ...body,
+        thumbnail: secure_url,
+        userId: userId
+      },
+    })
+  } catch (error) {
+    throw error;
+  }
+};
